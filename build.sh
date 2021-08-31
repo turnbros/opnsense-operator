d5f60f67eb0faa376fd7d7453c7b6e705e2334fa
Dylan Turnbull <dturbull@seismic.com>
2021-08-31 10:54:52 -0500

    made some progress

---
+++ build.sh
@@ -0,0 +1,2 @@
+make bundle IMG="ghcr.io/turnbros/opnsense-operator/opnsense-operator:v0.0.1"
+make bundle-build bundle-push BUNDLE_IMG="ghcr.io/turnbros/opnsense-operator/opnsense-operator:v0.0.1" 
